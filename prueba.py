5315694b4191bb8325f423d67dd23557ccbcd5a5
Diego Silva Cordoba <diegosilvacordoba@hotmail.com>
2022-07-28 17:03:13 +0200

    28.07.2022

--- prueba.py
+++ prueba.py
@@ -27,29 +27,29 @@
 #PROYECTO 2
 # Adivinar un numero generado por la computadora, sera interactivo. El usuario adivinar un numero en un rango especifico y le vamos a decir si ese numoer en mayor, menor o igual al numero aleatorio generado por la computadora.
 
-import random
+# import random
 
-def adivina_el_numero(x):
+# def adivina_el_numero(x):
     
     
-    print("=========================")
-    print("¡Bienvenido(a) al juego! ")
-    print("=========================")
-    print ("Tu meta es adivinar el numero generado por la computadora.")
+#     print("=========================")
+#     print("¡Bienvenido(a) al juego! ")
+#     print("=========================")
+#     print ("Tu meta es adivinar el numero generado por la computadora.")
     
-    numero_aleatorio = random.randint(1, x) # Numero aleatorio entre 1 y x.
-    prediccion = 0
-    while prediccion != numero_aleatorio:
-        # El usuario ingresa un numero
-        prediccion = int(input(f"Adivina un numero entre 1 y {x}: ")) # f-string
+#     numero_aleatorio = random.randint(1, x) # Numero aleatorio entre 1 y x.
+#     prediccion = 0
+#     while prediccion != numero_aleatorio:
+#         # El usuario ingresa un numero
+#         prediccion = int(input(f"Adivina un numero entre 1 y {x}: ")) # f-string
         
-    if prediccion < numero_aleatorio:
-        print("Intenta  otra vez. Este numero es muy pequeño. ")
-    elif prediccion > numero_aleatorio:
-        print("Intenta otra vez. Este numero es muy grande. ")
+#     if prediccion < numero_aleatorio:
+#         print("Intenta  otra vez. Este numero es muy pequeño. ")
+#     elif prediccion > numero_aleatorio:
+#         print("Intenta otra vez. Este numero es muy grande. ")
 
 
-    print(f"¡Felicitaciones! Adivinaste el numero {numero_aleatorio} correctamente. ")
+#     print(f"¡Felicitaciones! Adivinaste el numero {numero_aleatorio} correctamente. ")
 
 
-adivina_el_numero(10)
+# adivina_el_numero(10)
